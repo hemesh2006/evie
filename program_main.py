46381613cd18ad5c6790e508f4bc7bf93f2f199e
unknown <hemesh746@gmail.com>
2024-11-08 20:10:48 +0530

    st1

--- program_main.py
+++ program_main.py
@@ -34,6 +34,7 @@ from components.json_folder.jsonmanager import *
 '''
 '''json sample image:
 [["skull.png", [100, 200], 5, 500]]'''
+import time
 text=JSON("components\\json_folder\\text.json")
 image=JSON("components\\json_folder\\images.json")
 input=JSON("components\\json_folder\\inputs.json")
@@ -44,6 +45,36 @@ def init_screen():
     input.clear()
     gif.clear()
     text.write(["evie",[860,380],70,"green"])
+    time.sleep(2)
+    text.append([".",[1090,330],100,"green"])
+    time.sleep(2)
+    text.append([".",[1150,330],100,"green"])
+    time.sleep(2)
+    text.append([".",[1200,330],100,"green"])
+    time.sleep(0.4)
+    text.remove_element(".")
+    text.remove_element(".")
+    text.remove_element(".")
+    time.sleep(2)
+    text.append([".",[1090,330],100,"green"])
+    time.sleep(2)
+    text.append([".",[1150,330],100,"green"])
+    time.sleep(2)
+    text.append([".",[1200,330],100,"green"])
+    time.sleep(2)
+    text.append(["initializing",[850,540],35,"green"])
+    time.sleep(1)
+    text.remove_element("initializing")
+    time.sleep(1)
+    text.append(["initializing",[850,540],35,"green"])
+    time.sleep(1)
+    text.remove_element("initializing")
+    time.sleep(1)
+    text.append(["initializing",[850,540],35,"green"])
+    time.sleep(1)
+    text.remove_element("initializing")
+    time.sleep(1)
+
 
 
 init_screen()
